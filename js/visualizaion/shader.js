2c498315538a3bc0e7daa48da631a9e5f2ac79d6
sshusain <sshusain@umich.edu>
2015-09-14 00:54:22 -0400

    implement pass-through vertex simulation shader

--- js/visualizaion/shader.js
+++ js/visualizaion/shader.js
@@ -5,3 +5,15 @@
 //closure commands
 goog.provide('DVT.shader');
 
+DVT.ParticleSimulationV = [
+
+    "varying vec2 vUv;",
+
+    "void main() {",
+
+    "vUv = vec2(uv.x, 1.0 - uv.y);",
+    "gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );",
+
+    "} "
+
+].join("\n");
