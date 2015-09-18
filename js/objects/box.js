71e65fe75f4e336d32a544e07841527e63b09af4
sshusain <sshusain@umich.edu>
2015-09-18 09:50:51 -0400

    implement default assignment operator

--- js/objects/box.js
+++ js/objects/box.js
@@ -52,3 +52,79 @@ DVT.box = function(copyFrom) {
 };
 
 goog.inherits(DVT.box, DVT.loaded);
+
+/**
+ * Get the box length.
+ *
+ * @return {number} length of the box
+ * @public
+ */
+DVT.box.prototype.__defineGetter__('length', function() {
+
+    return this._length;
+
+});
+
+/**
+ * Set the length of the object
+ *
+ * @param {number} integer-valued length
+ * @public
+ */
+DVT.renderer3D.prototype.__defineSetter__('length', function(length) {
+
+    this._length = length;
+    return this._length;
+});
+
+
+
+/**
+ * Get the box width.
+ *
+ * @return {number} width of the box
+ * @public
+ */
+DVT.box.prototype.__defineGetter__('width', function() {
+
+    return this._width;
+
+});
+
+/**
+ * Set the width of the object
+ *
+ * @param {number} integer-valued width
+ * @public
+ */
+DVT.renderer3D.prototype.__defineSetter__('width', function(width) {
+
+    this._width = width;
+    return this._width;
+});
+
+
+
+/**
+ * Get the box depth.
+ *
+ * @return {number} depth of the box
+ * @public
+ */
+DVT.box.prototype.__defineGetter__('depth', function() {
+
+    return this._depth;
+
+});
+
+/**
+ * Set the depth of the object
+ *
+ * @param {number} integer-valued depth
+ * @public
+ */
+DVT.renderer3D.prototype.__defineSetter__('depth', function(depth) {
+
+    this._depth = depth;
+    return this._depth;
+});
