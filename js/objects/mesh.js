85b647367dc072eee311c8799147f9c4b33a3e60
Unknown <sshusain@umich.edu>
2018-04-10 04:15:40 -0400

    Add voronoi support for meshes

--- js/objects/mesh.js
+++ js/objects/mesh.js
@@ -34,6 +34,11 @@ DVT.mesh = function(copyFrom) {
     
     this._expandAnimate = false;
     
+    
+    this._voronoiSystem = null;
+    
+    this._voronoiIndex = 0;
+    this._voronoiContainer = null;
 
 };
 goog.inherits(DVT.mesh, DVT.loaded);
@@ -58,11 +63,46 @@ DVT.mesh.prototype.init = function (renderer) {
     
     if(this._voronoiSystem)
     {
-            
+        this.calcVoronoi();
     }
 };
 
 DVT.mesh.prototype.calcVoronoi = function()
 {
+    console.log(this._voronoiSystem);
+    var geomClone = this._voronoiContainer.geometry.clone();
+    var geom = new THREE.Geometry().fromBufferGeometry( geomClone );
+    delete geomClone;
+    var curPoint = this._voronoiSystem[this._voronoiIndex]
+    for(var j=0;j<this._voronoiSystem.length;j++)
+    {
+        if(j!=this._voronoiIndex)
+        {
+            var normal = new THREE.Vector3(curPoint.x, curPoint.y, curPoint.z);
+            normal.sub(this._voronoiSystem[j]);
+            normal.divideScalar(2);
+            
+            var plane = new THREE.Plane(normal, 0);
+            
+            normal = new THREE.Vector3(curPoint.x, curPoint.y, curPoint.z);
+            
+            
+            normal.add(this._voronoiSystem[j]);
+            normal.divideScalar(2);
+            
+            plane.translate(normal);
+            
+            geom = sliceGeometry(geom, plane, true);
+            
+            geom.computeFaceNormals();
+            geom.computeVertexNormals();
+        }
+    }
+    geom.computeFaceNormals();
+    geom = new THREE.BufferGeometry().fromGeometry(geom);
+    //create material
+    var material = new THREE.MeshPhongMaterial({color:this._color});
+
+    this.THREEContainer = new THREE.Mesh(geom, material);
     
 }
