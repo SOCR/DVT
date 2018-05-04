bf46da16a70fe7af486f4ceccef4445ec6b46e58
Unknown <sshusain@umich.edu>
2018-05-03 21:26:05 -0400

    Change test bed to use OFF files

--- test/js/testFSM.js
+++ test/js/testFSM.js
@@ -11,11 +11,32 @@ test = function() {
 
     var lh = new DVT.mesh();
     // .. attach a Freesurfer .smoothwm mesh
-    lh.file = 'http://x.babymri.org/?rh.smoothwm';
+    lh.file = 'data/cell1.off';
     // change the color to a smooth red
     lh.color = [0.7, 0.2, 0.2];
     // add some transparency
     lh.opacity = 0.6;
+    var lh2 = new DVT.mesh();
+    // .. attach a Freesurfer .smoothwm mesh
+    lh2.file = 'data/cell2.off';
+    // change the color to a smooth red
+    lh2.color = [0.7, 0.2, 0.2];
+    // add some transparency
+    lh2.opacity = 0.6;
+    var lh3 = new DVT.mesh();
+    // .. attach a Freesurfer .smoothwm mesh
+    lh3.file = 'data/cell3.off';
+    // change the color to a smooth red
+    lh3.color = [0.7, 0.2, 0.2];
+    // add some transparency
+    lh3.opacity = 0.6;
+    var lh4 = new DVT.mesh();
+    // .. attach a Freesurfer .smoothwm mesh
+    lh4.file = 'data/cell4.off';
+    // change the color to a smooth red
+    lh4.color = [0.7, 0.2, 0.2];
+    // add some transparency
+    lh4.opacity = 0.6;
 
 
     var porsche = new DVT.mesh();
@@ -48,9 +69,12 @@ test = function() {
     // [1,1,1] (== white) is also the default so this can be skipped aswell
     cube.color = [1, 1, 1];
 
-    test_renderer.add(cube);
+    //test_renderer.add(cube);
     //test_renderer.add(porsche);
-    //test_renderer.add(lh);
+    test_renderer.add(lh);
+    test_renderer.add(lh2);
+    test_renderer.add(lh3);
+    test_renderer.add(lh4);
     //test_renderer.add(molecule);
     //test_renderer.add(molecule2);
 
