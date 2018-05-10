caa336bfeaca4a28794cb349bc0e15d93c04c448
sshusain <sshusain@umich.edu>
2018-05-10 15:00:00 -0400

    Add subdivision modifier

--- dist/deps.js
+++ dist/deps.js
@@ -8,7 +8,7 @@ goog.addDependency('../../../../../../js/io/loader/loadHelper.js', ['DVT.loadHel
 goog.addDependency('../../../../../../js/io/loader/loader.js', ['DVT.loader'], ['DVT', 'DVT.loadHelper', 'jQuery'], false);
 goog.addDependency('../../../../../../js/io/parser/parser.js', ['DVT.parser'], ['DVT.base'], false);
 goog.addDependency('../../../../../../js/io/parser/parserFSM.js', ['DVT.parserFSM'], ['DVT.parser', 'THREE'], false);
-goog.addDependency('../../../../../../js/io/parser/parserGIF.js', ['DVT.parserGIF'], ['DVT.parser', 'THREE', 'gifuct', 'voxel-mesh'], false);
+goog.addDependency('../../../../../../js/io/parser/parserGIF.js', ['DVT.parserGIF'], ['DVT.parser', 'THREE', 'THREE.SubdivisionModifier', 'gifuct', 'surface-nets', 'voxel-mesh'], false);
 goog.addDependency('../../../../../../js/io/parser/parserOFF.js', ['DVT.parserOFF'], ['DVT.parser', 'THREE'], false);
 goog.addDependency('../../../../../../js/io/parser/parserPDB.js', ['DVT.parserPDB'], ['DVT.parser', 'THREE', 'THREE.PDBUtil'], false);
 goog.addDependency('../../../../../../js/io/parser/parserSTL.js', ['DVT.parserSTL'], ['DVT.parser', 'THREE'], false);
@@ -24,6 +24,8 @@ goog.addDependency('../../../../../../js/lib/gifuct.js', ['gifuct'], [], false);
 goog.addDependency('../../../../../../js/lib/jquery.min.js', ['jQuery'], [], false);
 goog.addDependency('../../../../../../js/lib/progressbar.min.js', ['ProgressBar'], [], false);
 goog.addDependency('../../../../../../js/lib/slice.min.js', ['THREE.slice'], ['THREE'], false);
+goog.addDependency('../../../../../../js/lib/subdivisionModifier.js', ['THREE.SubdivisionModifier'], ['THREE'], false);
+goog.addDependency('../../../../../../js/lib/surfacenets.js', ['surface-nets'], ['THREE'], false);
 goog.addDependency('../../../../../../js/lib/three.min.js', ['THREE'], [], false);
 goog.addDependency('../../../../../../js/lib/voxel-mesh.js', ['voxel-mesh'], ['THREE', 'voxel'], false);
 goog.addDependency('../../../../../../js/lib/voxel.js', ['voxel'], [], false);
